ec26864f8aafda8b49cd4e49c3dc03f4e6efdcb1
sanzid <sanzid@m2sys.com>
2025-10-24 19:54:45 +0600

    Documentation has been updated in AuthConfiguration..

--- DemoAuthCloudApper/AuthConfiguration.swift
+++ DemoAuthCloudApper/AuthConfiguration.swift
@@ -6,6 +6,7 @@
 //
 
 struct AuthConfiguration {
+    // Note: The information in this document is provided by CloudApper AI.
     static let baseUrl = "" // The base URL of the identity provider (authorization server).
     static let authUri = "" // The path to the authorization endpoint, appended to baseUrl.
     static let tokenUri = "" // The path to the token endpoint, used for exchanging codes for tokens.
@@ -15,6 +16,6 @@ struct AuthConfiguration {
     static let clientSecret = "" // The client secret (often empty for public clients like mobile apps).
     static let callbackUrl = "" // The redirect URI the provider sends the authorization code to (must be registered).
     static let logoutCallbackUrl = "" // The redirect URI the provider sends the user to after logging out.
-    static let servicename = "" // A service name, often used for storing credentials (e.g., in Keychain).
+    static let servicename = "" // A service name, often used for showing branding app .
     static let responseType = ""
 }
